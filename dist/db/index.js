f089ff7c5bfc1b4f54254d36ad04675dbb467fda
Ulises Castro <ulisescastro2015@gmail.com>
2020-03-29 19:39:49 -0600

    Fix db never connect successfully

--- dist/db/index.js
+++ dist/db/index.js
@@ -10,11 +10,9 @@ var autoIndex = !!process.env.PRODUCTION;
 
 // Defining vars to connect to database
 var options = {
+  authSource: 'admin',
   user: process.env.DATABASE_USER,
   pass: process.env.DATABASE_PWD,
-  auth: {
-    authdb: 'admin'
-  },
   useNewUrlParser: true,
   useCreateIndex: true,
   autoIndex: autoIndex,
