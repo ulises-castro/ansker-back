037f0f62dabe48f453a5bb06aa25da37ebf080ea
Ulises Castro <ulisescastro2015@gmail.com>
2020-04-12 16:09:34 -0500

    Moving listening app to www/bin and active config

--- server/bin/www.js
+++ server/bin/www.js
@@ -6,51 +6,63 @@
  */
 
 
-import app from '../app';
-import debugLib from 'debug';
-import http from 'http';
-const debug = debugLib('ansker-back:server');
+import app from '../app'
+import debugLib from 'debug'
+import http from 'http'
+import https from 'https'
+
+import fs from 'fs'
+const debug = debugLib('ansker-back:server')
 // ..generated code b
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+// var server = http.createServer(app)
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+const ssl = {
+  key: fs.readFileSync('/home/alex/ssl/mydev.dev+5-key.pem'),
+  cert: fs.readFileSync('/home/alex/ssl/mydev.dev+5.pem')
+}
+
+const choiceProtocol = !JSON.parse(process.env.PRODUCTION) ? [https, ssl] : [http, {}]
+
+const server = https.createServer(choiceProtocol[1], app)
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  var port = parseInt(val, 10)
 
   if (isNaN(port)) {
     // named pipe
-    return val;
+    return val
   }
 
   if (port >= 0) {
     // port number
-    return port;
+    return port
   }
 
-  return false;
+  return false
 }
 
 /**
@@ -59,25 +71,25 @@ function normalizePort(val) {
 
 function onError(error) {
   if (error.syscall !== 'listen') {
-    throw error;
+    throw error
   }
 
   var bind = typeof port === 'string' ?
     'Pipe ' + port :
-    'Port ' + port;
+    'Port ' + port
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
     default:
-      throw error;
+      throw error
   }
 }
 
@@ -86,9 +98,9 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
+  var addr = server.address()
   var bind = typeof addr === 'string' ?
     'pipe ' + addr :
-    'port ' + addr.port;
-  debug('Listening on ' + bind);
+    'port ' + addr.port
+  debug('Listening on ' + bind)
 }
