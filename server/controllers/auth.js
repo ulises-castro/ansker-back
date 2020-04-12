3e892607f2a4464fb718810db2b95b184327a40f
Ulises Castro <ulisescastro2015@gmail.com>
2020-04-12 14:59:32 -0500

    Separete facebook and google auth into Auth folders

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,20 @@
+'use strict'
+import to from 'await-to-js'
+import axios from 'axios'
+
+import User from 'models/user'
 const jwt = require('jsonwebtoken')
 const passportJWT = require('passport-jwt')
 const ExtractJwt = passportJWT.ExtractJwt
 
 const jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+
 jwtOptions.secret = process.env.JWT_SECRET_PASSWORD
 
-import joinOrLoginFacebook from '../../auth/facebook-auth.js'
+import joinOrLoginFacebook from 'auth/facebook-auth.js'
 
-const loginFacebook = async (req, res, next) => {
+async function loginFacebook  (req, res, next) {
   const { tokenFB } = req.body
 
   let payload = {
@@ -57,4 +63,86 @@ const loginFacebook = async (req, res, next) => {
   } else {
     return res.status(403).json(sendInvalidUser)
   }
+}
+
+const googleInfoByToken = async (req, res, next) => {
+  const {
+    access_token
+  } = req.query
+
+  const [ err, googleInfoByTokenData ] = await to(axios({
+    url: 'https://www.googleapis.com/oauth2/v2/userinfo',
+    method: 'get',
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  }))
+
+  console.log(googleInfoByTokenData) // { id, email, given_name, family_name }
+
+  console.log(err)
+
+  if (err) next(err)
+  const { data } = googleInfoByTokenData
+
+  data.token = access_token
+
+  data.ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+  registerOrLoginUser(data, res, next)
+}
+
+async function registerOrLoginUser(userData, res, next) {
+  userData.verified = userData.verified_email
+
+  const [err, newUser] = await to(User.findUserOrRegister(
+    userData,
+    'google'
+  ))
+
+  if (err) next(err)
+
+  console.log(err, newUser)
+
+  const token = jwt.sign(
+    newUser.id, jwtOptions.secret
+  )
+
+  res.status(200).json({
+    token
+  })
+}
+const loginGoogle = async (req, res, next) => {
+  const {
+    code
+  } = req.query
+
+  const [ err, googleAuthData ] = await to(axios({
+      url: `https://oauth2.googleapis.com/token`,
+      method: 'post',
+      data: {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri: `${process.env.URL_FRONT}/authenticate/google`,
+        grant_type: 'authorization_code',
+        code
+      },
+    }))
+
+    // console.log(googleAuthData)
+    if (googleAuthData) {
+      const { data } = googleAuthData
+
+      res.status(200).json({
+        ...data
+      })
+    }
+
+    next(err)
+}
+
+export {
+  loginFacebook,
+  loginGoogle,
+  googleInfoByToken,
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -8,14 +8,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// Google auth ---------------------------------
-// Geting token sending code
-// app.post('/api/autheticate/google/token', userController.requestGmailAuth)
-// Get code
-router.get('/authenticate/google', userController.getAccessTokenFromCode)
-
-// Get google user
-router.get('/authenticate/google/token', userController.getGoogleInfo)
-// ------------------------------------------
-
 module.exports = router
