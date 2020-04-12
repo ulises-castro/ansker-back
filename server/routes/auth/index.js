0e38a33299148b6b30ef515f6d89b3d98b533c85
Ulises Castro <ulisescastro2015@gmail.com>
2020-04-12 14:59:32 -0500

    Separete facebook and google auth into Auth folders

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-import { loginFacebook } from 'controllers/auth'
+import { loginFacebook, loginGoogle, googleInfoByToken } from 'controllers/auth'
 
 // Post login/ Register login vía facebook\
 router.post('/facebook', loginFacebook)
 
-// router.post('/google', Auth.loginGoogle)
+// Get code
+router.get('/google', loginGoogle)
 
-export default router
+// Get google user
+router.get('/google/token', googleInfoByToken)
+
+module.exports = router
