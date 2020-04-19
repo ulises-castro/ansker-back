62d63057ad41bdd11e7f2295f693b761ac129e44
Ulises Castro <ulisescastro2015@gmail.com>
2020-04-18 23:45:49 -0500

    Added new files to get countries

---
+++ server/routes/country.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+import { getCity, getCountry } from 'controllers/country'
+
+router.get('city/:city', getCity)
+
+router.get('country/:country', getCountry)
+
+module.exports = router
