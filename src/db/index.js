048310cc61cfe6e9507322a5183d6261497b3655
Ulises Castro <ulisescastro2015@gmail.com>
2020-05-23 16:00:26 -0500

    Added config to connect to db

--- src/db/index.js
+++ src/db/index.js
@@ -1,18 +1,21 @@
-;
-
-const mongoose = require('mongoose');
-
-// const uri = 'mongodb://587f9fd4b7b2:27017/ansker';
-const uri = 'mongodb://0.0.0.0:27017/ansker';
 
 
-const autoIndex = !!process.env.PRODUCTION;
+const mongoose = require('mongoose')
+
+// const uri = 'mongodb://587f9fd4b7b2:27017/ansker'
+const uri = 'mongodb://0.0.0.0:27017/ansker'
+
+// const uri = 'mongodb://srv-captain--ansker-mongodb/ansker'
+
+
+const autoIndex = !!process.env.PRODUCTION
 
 // Defining vars to connect to database
 const options = {
   // authSource: 'admin',
   // user: process.env.DATABASE_USER,
   // pass: process.env.DATABASE_PWD,
+  // userMongoClient: true,
   useNewUrlParser: true,
   useCreateIndex: true,
   autoIndex,
@@ -20,10 +23,10 @@ const options = {
   serverSelectionTimeoutMS: 5000
   // reconnectTries: 30,
   // reconnectInterval: 500, // in ms
-};
+}
 
 mongoose.connect(uri, options).then(() => {
-  console.log('connected to database');
+  console.log('connected to database')
 }).catch(error => {
-  console.log('There is a problem with database', error);
-});
+  console.log('There is a problem with database', error)
+})
