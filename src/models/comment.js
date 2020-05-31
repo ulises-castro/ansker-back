08518fea3d1bc462d26cfd0c7ef4e161cbfe11cd
Ulises Castro <ulisescastro2015@gmail.com>
2020-05-30 23:38:28 -0500

    remove useless code

--- src/models/comment.js
+++ src/models/comment.js
@@ -84,25 +84,3 @@ CommentSchema.statics.getAll = async function (publicationId,userId) {
 CommentSchema.set('toJSON', { getters: true, virtuals: true })
 
 module.exports = mongoose.model('Comments', CommentSchema)
-
-  // TODO: Implement avatars functionalities
-  // const userHasCommented = await publication
-  // .findOne({
-  //   '_id' : publication._id,
-  //   'commentsAuthors.authorId': author
-  // }).exec()
-
-  // // Register authors and asign random avatar as their identify
-  // if (!userHasCommented) {
-  //   // Modify this to make dinamic to assign inrepeateable avatar
-  //   const availableAvatars = 30
-  //   const avatar =  Math.floor(Math.random() * (availableAvatars)) + 1
-
-  //   publication.commentsAuthors.push({
-  //     author,
-  //     avatar,
-  //     backgroundColor,
-  //   })
-
-  //   await publication.save().then(comment => comment)
-  // }
