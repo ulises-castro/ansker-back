00c7ba13d91413e35bb97ac434da98823442f17c
Ulises Castro <ulisescastro2015@gmail.com>
2020-06-11 22:50:35 -0500

    Created questions file

---
+++ src/routes/question.js
@@ -0,0 +1,24 @@
+// const express = require('express')
+// const router = express.Router()
+
+// const passport = require('passport')
+
+// import { getAllByCategory, getQuestion, voteUp, voteDown, publish } from 'controllers/question'
+
+// router.post('/publish', passport.authenticate('jwt', {
+//   session: false,
+// }), publish)
+
+// router.post('/voteUp', passport.authenticate('jwt', {
+//   session: false,
+// }), voteUp)
+
+// router.get('/:questionId', passport.authenticate('jwt', {
+//   session: false,
+// }), getQuestion)
+
+// router.get('/category/:category/:pageNumber', getAllByCategory)
+
+// router.get('/filter/all/:pageNumber', getAll)
+
+// module.exports = router
