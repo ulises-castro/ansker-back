3c31c6b9963c8f00ab1ad1eb21166eb804e7fed5
Ulises Castro <ulisescastro2015@gmail.com>
2020-06-20 11:02:24 -0500

    Adding rules into push notifications

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -25,12 +25,30 @@ export const suscribeUserToTopic = async (userId, topic) => {
   }
 }
 
-export const notifyNewComments = async (publicationId) => {
+export const notifyNewComments = async (publicationId, isUserAuthor) => {
   const messages = []
 
+  if (!isUserAuthor) {
+    messages.push({
+      notification: {title: 'New comment', body: 'Someone has commented on your publication.'},
+      "collapse_key": `publication-${publicationId}`,
+      topic: `user-publication-${publicationId}`,
+      "webpush": {
+        "headers": {
+          "Urgency": "high"
+        }
+      }
+    })
+  }
+
+  const choiceSubject = (!isUserAuthor) ? 'Someone' : 'Author'
+
   messages.push({
-    notification: {title: 'New comment', body: 'Someone has commented on your publication.'},
-    topic: `user-publication-${publicationId}`,
+    notification: {
+      title: 'New comment', body: `${choiceSubject} has commented on a publication that you commented as well.`}
+    ,
+    "collapse_key": `publication-${publicationId}`,
+    topic: `publication-${publicationId}`,
     "webpush": {
       "headers": {
         "Urgency": "high"
@@ -38,8 +56,58 @@ export const notifyNewComments = async (publicationId) => {
     }
   })
 
+  firebaseAdmin.messaging().sendAll(messages)
+  .then((response) => {
+    console.log(response.successCount + ' messages were sent successfully')
+  })
+
+}
+
+// Profile 
+export const notifyProfilePublication = async (profileId, userName = false, isUserAuthor) => {
+  const messages = []
+
+  if (!isUserAuthor) {
+    messages.push({
+      notification: {title: 'New publication on your profile', body: 'Someone has publicated on your profile.'},
+      "collapse_key": `profile-${profileId}`,
+      topic: `user-profile-${profileId}`,
+      "webpush": {
+        "headers": {
+          "Urgency": "high"
+        }
+      }
+    })
+  }
+
+  firebaseAdmin.messaging().sendAll(messages)
+  .then((response) => {
+    console.log(response.successCount + ' messages were sent successfully')
+  })
+
+}
+
+// QUESTIONS SECTION
+export const notifyNewQuestions = async (publicationId, isUserAuthor) => {
+  const messages = []
+
+
+  if (!isUserAuthor) {
+    messages.push({
+      notification: {title: 'New comment', body: 'Someone has commented on your publication.'},
+      "collapse_key": `publication-${publicationId}`,
+      topic: `user-publication-${publicationId}`,
+      "webpush": {
+        "headers": {
+          "Urgency": "high"
+        }
+      }
+    })
+  }
+
   messages.push({
     notification: {title: 'New comment', body: 'Someone has commented on a publication that you commented as well.'},
+    "collapse_key": `publication-${publicationId}`,
     topic: `publication-${publicationId}`,
     "webpush": {
       "headers": {
